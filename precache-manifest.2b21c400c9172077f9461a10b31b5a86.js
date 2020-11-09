9c0b9da0cf69bfaeafec66befa346f4662704bd0
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-11-09 12:07:22 +0200

    Updates

---
+++ precache-manifest.2b21c400c9172077f9461a10b31b5a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1742c5f6169036b23cb4a78068df8b2b",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "ff950c00ebf81dc6d420",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.c175b3ad.chunk.css"
+  },
+  {
+    "revision": "1b6440245dbcbcdec835",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.c4ea2c88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.c4ea2c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff950c00ebf81dc6d420",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.6eff5df8.chunk.js"
+  },
+  {
+    "revision": "fd44328cb7a21a2e0a73",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b930395d.js"
+  },
+  {
+    "revision": "c139f5c832f77eb95ede936d4db58a68",
+    "url": "/goit-react-hw-06-phonebook/static/media/trash.c139f5c8.png"
+  }
+]);
